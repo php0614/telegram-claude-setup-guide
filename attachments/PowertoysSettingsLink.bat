2ca81a0f242fb61b9d7b369b53e76a4636b831eb
phpaqr <elecoustics@gmail.com>
2026-04-24 15:12:05 +0900

    Auto-update: 2026-04-24 15:12:04

---
+++ attachments/PowertoysSettingsLink.bat
@@ -0,0 +1,17 @@
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\Image Resizer"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\Image Resizer" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\Image Resizer"
+
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\FancyZones"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\FancyZones" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\FancyZones"
+
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\Keyboard Manager"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\Keyboard Manager" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\Keyboard Manager"
+
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\PowerToys Run"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\PowerToys Run" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\PowerToys Run"
+
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\AlwaysOnTop"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\AlwaysOnTop" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\AlwaysOnTop"
+
+RD /S /Q "C:\Users\PL\AppData\Local\Microsoft\PowerToys\TextExtractor"
+mklink /j "C:\Users\PL\AppData\Local\Microsoft\PowerToys\TextExtractor" "D:\Dropbox\SettingFiles\_Win\PowertoysSettings\TextExtractor"
